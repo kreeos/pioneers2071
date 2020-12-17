3d16a80a602ca57cd005655543300dba9c82d35e
CheddarChoi <cde1103@kaist.ac.kr>
2020-12-17 14:06:09 +0900

    fix typo

--- frontend/src/components/Complete.js
+++ frontend/src/components/Complete.js
@@ -1,11 +1,10 @@
-import './Complete.css';
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import "./Complete.css";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Complete extends Component {
   constructor(props) {
     super(props);
-
   }
 
   render() {
@@ -13,21 +12,21 @@ class Complete extends Component {
       <div className="Complete">
         <div className="complete-container-whole">
           <div className="complete-title-div">
-            <a>
-              Application
-            </a>
+            <a>Application</a>
           </div>
           <div className="complete-main-div">
             <a>
-              Application Submitted!<br/>
-              If you want to edit your registration information,<br/>
-              Please contact us by E-mail (kaistua@kaist.ac.kr)            
+              Application Submitted!
+              <br />
+              If you want to edit your registration information,
+              <br />
+              Please contact us by E-mail (kaistua@gmail.com)
             </a>
           </div>
           <div className="complete-container">
-          <Link to="/home">
-            <button className="btn-join">Back to Home</button>
-          </Link>
+            <Link to="/home">
+              <button className="btn-join">Back to Home</button>
+            </Link>
           </div>
         </div>
       </div>
