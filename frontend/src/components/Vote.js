b882545350160340c3fc86109dcee0f7e455be1f
CheddarChoi <cde1103@kaist.ac.kr>
2021-01-31 23:35:09 +0900

    complete vote main page

--- frontend/src/components/Vote.js
+++ frontend/src/components/Vote.js
@@ -1,7 +1,7 @@
 import "./VotePage.css";
 import React, { Component } from "react";
 import { Card, Button } from "react-bootstrap";
-import { FaAngleRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 import Climate from "../images/Climate.gif";
 import AI from "../images/AI.gif";
@@ -12,42 +12,28 @@ class Vote extends Component {
       <div className="Vote">
         <div className="container mb-5">
           <div className="vote-title-div">
-            <a>About Pioneers 2071</a>
+            <a>Semi-Final Vote</a>
           </div>
           <div className="vote-main-div">
-            <div className="d-flex justify-content-center row">
-              <Card
-                style={{
-                  width: "30%",
-                  border: "none",
-                  backgroundColor: "rgba(0,0,0,0)",
-                }}
-              >
+            <div className="vote-cards-container">
+              <Card className="vote-card">
                 <Card.Img variant="top" src={Climate} />
-                <Card.Body>
-                  <Card.Title>Society Made of Burnt Fuels</Card.Title>
-                  <button
-                    type="submit"
-                    class="btn custom-btn mt-3"
-                    value="Submit"
-                  >
-                    Vote
-                    <FaAngleRight />
-                  </button>
-                </Card.Body>
+                <div className="vote-card-title">
+                  Society Made of
+                  <br /> Burnt Fuels
+                </div>
+                <Link to="/vote/semi/env">
+                  <button className="vote-btn-join mb-3">Vote</button>
+                </Link>
               </Card>
-              <Card
-                style={{
-                  width: "30%",
-                  border: "none",
-                  backgroundColor: "rgba(0,0,0,0)",
-                }}
-              >
+              <Card className="vote-card">
                 <Card.Img variant="top" src={AI} />
-                <Card.Body>
-                  <Card.Title>The World of an "Intelligent" Mind</Card.Title>
-                  <Button variant="primary">Vote</Button>
-                </Card.Body>
+                <div className="vote-card-title">
+                  The World of an <br /> "Intelligent" Mind
+                </div>
+                <Link to="/vote/semi/ai">
+                  <button className="vote-btn-join mb-3">Vote</button>
+                </Link>
               </Card>
             </div>
           </div>
